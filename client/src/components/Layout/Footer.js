e2be150ce98fec9ceec76a0efd92f41369621453
Nitish Nashine <nitishnashine@gmail.com>
2023-06-25 12:29:16 +0530

    basic folder structure and routing done

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center footer">
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
