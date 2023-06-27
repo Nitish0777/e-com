6a056429e4fab766c229510f3a6de15de5a1128e
Nitish Nashine <nitishnashine@gmail.com>
2023-06-27 11:27:08 +0530

    search bar design fixed

---
+++ client/src/pages/Auth/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/login.css";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Handle login logic
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
+      if (res.data.success) {
+        toast.success("Register successfull");
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log("Login error", error);
+      toast.error("Login error");
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
