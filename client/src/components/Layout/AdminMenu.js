b3e7f7d5854216369b6c3c79deb28882d00c04bd
Nitish Nashine <nitishnashine@gmail.com>
2023-06-29 17:50:04 +0530

    working on admin panel

---
+++ client/src/components/Layout/AdminMenu.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <>
+      <div className="text-center">
+        <div className="list-group">
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action bg-light"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action bg-light"
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/users"
+            className="list-group-item list-group-item-action bg-light"
+          >
+            Users
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminMenu;
