3ef82d455ddc7ec97a2102daf8768b9f07fbc058
Nitish Nashine <nitishnashine@gmail.com>
2023-06-29 23:25:36 +0530

    Admin dashboard somewhat completed

---
+++ client/src/user/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import UserMenu from "../components/Layout/UserMenu";
+
+const Profile = () => {
+  return (
+    <Layout title={"User Profile"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3 offset-md-3">
+            <UserMenu />
+          </div>
+
+          <h4 className="col-md-9">Profile</h4>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Profile;
