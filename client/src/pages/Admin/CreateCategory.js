0ff455bf29b05a538f21400ad74dcdfc98aae544
Nitish Nashine <nitishnashine@gmail.com>
2023-07-01 19:06:55 +0530

    creating by form  category done

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -3,9 +3,33 @@ import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import { toast } from "react-hot-toast";
 import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  //handle form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/category/create-category`,
+        {
+          name,
+        }
+      );
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        setName("");
+        getCategories();
+      } else {
+        toast.error(`${name} is not created`);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error in form for creating category");
+    }
+  };
 
   //get categories from backend
   const getCategories = async () => {
@@ -33,6 +57,13 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
             <div className="w-75">
               <table className="table">
                 <thead>
